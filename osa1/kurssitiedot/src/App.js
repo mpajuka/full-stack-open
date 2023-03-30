c69070b76a7938dde76f9af28d22eb9f180411ce
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-03-30 20:34:37 +0300

    finish 1.4

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -31,33 +31,40 @@ const Part = (props) => {
 
 const Total = (props) => {
   console.log(props)
+  let total_exercises = 0
+  for (let i = 0; i < props.total.length; ++i)
+  {
+    total_exercises += props.total[i].exercises
+  }
   return (
     <div>
-      <p>Number of exercises {props.total_number}</p>
+      <p>Number of exercises {total_exercises}</p>
     </div>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content contents={[part1, part2, part3]} />
-      <Total total_number={part1.exercises+part2.exercises+part3.exercises}/>
+      <Content contents={parts} />
+      <Total total={parts}/>
     </div>
   )
 }
