259a7c09791a9aa26b56db30ad4b4cec6d010911
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-04-02 19:41:24 +0300

    finish 2.1 and 2.2 kurssitiedot, steps 6 and 7

---
+++ osa2/kurssitiedot/src/components/Course.js
@@ -0,0 +1,41 @@
+
+
+
+const Part = ({ part }) => {
+    return (
+        <p>{part.name} {part.exercises}</p>
+    )
+}
+
+
+const Content = ({ content }) => {
+    let sumOfExercises = content.reduce((a, b) => a = a + b.exercises, 0)
+    return (
+        <div>
+            {content.map(part =>
+                <Part key={part.id} part={part} />
+            )}
+            <strong>total of {sumOfExercises} exercises</strong>
+        </div>
+    )
+}
+
+
+const Header = ({ header }) => {
+    return (
+        <div>
+            <h1>{header}</h1>
+        </div>
+    )
+}
+
+const Course = ({ course }) => {
+    return (
+        <div>
+            <Header header={course.name} />
+            <Content key={course.id} content={course.parts} />
+        </div>
+    )
+}
+
+export default Course
