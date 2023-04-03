e48bcb352cdc6a7f817a9ba20c59475132eae9a2
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-04-03 15:02:24 +0300

    finish 2.4 kurssitiedot step9 and also 2.5 'erillinen moduuli', as my implementation already had Course as a separate module inside components

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -21,7 +21,7 @@ const Content = ({ content }) => {
 const Header = ({ header }) => {
     return (
         <div>
-            <h1>{header}</h1>
+            <h2>{header}</h2>
         </div>
     )
 }
