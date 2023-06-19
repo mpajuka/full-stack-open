e66950b3e859dffae4f6fb0a2ef83f9d73d8b901
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-06-19 14:19:33 +0300

    finish 4.17 blogilistan laajennus, step5

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,17 +1,30 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const blog = new Blog({ ...body })
+  const user = await User.findById(body.userId)
+  const users = await User.find({})
+  const placeHolderUser = users[0]
+  const blog = new Blog({ ...body, user: user === null ? placeHolderUser.id : user.id })
 
   const savedBlog = await blog.save()
+  if (user !== null) {
+    user.blogs = user.blogs.concat(savedBlog.id)
+    await user.save()
+  } else {
+    placeHolderUser.blogs = placeHolderUser.blogs.concat(savedBlog.id)
+    await placeHolderUser.save()
+  }
+
   response.status(201).json(savedBlog)
 })
 
