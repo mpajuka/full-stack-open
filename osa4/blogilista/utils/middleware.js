435a37edc13d41d8c1b53d6ab30f8d6269b71f34
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-06-19 16:21:01 +0300

    finish 4.20 blogilistan laajennus, step8

--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -8,10 +8,18 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(400).json({ error: 'token missing or invalid' })
   }
-
   next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token =  authorization.replace('Bearer ', '')
+  }
+
+  next()
+}
+
 module.exports = {
-  errorHandler
+  errorHandler, tokenExtractor
 }
