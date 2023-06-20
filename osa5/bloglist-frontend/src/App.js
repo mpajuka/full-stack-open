adeac9df7c47a4d57be9d8feac3b38e038cdc92b
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-06-20 13:42:10 +0300

    finish 5.7 blogilistan frontend, step 7

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -129,12 +129,12 @@ const App = () => {
           handleUrlChange={({ target }) => setUrl(target.value)}
           handleSubmit={handleCreate}
         />
-      </Togglable> 
-      <ul>
+      </Togglable>
+      <div> 
         {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
+        <Blog key={blog.id} blog={blog} />
         )}
-      </ul>
+      </div>
     </div>
   )
 
--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-  <div>
-    <li>{blog.title} {blog.author}</li>
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const blogStyle = {
+    marginTop: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [blogVisible, setBlogVisible] = useState(false)
+
+  const toggleBlogVisible = (event) => {
+    setBlogVisible(!blogVisible)
+  }
+  if (blogVisible === true) {
+    return(
+      <div style={blogStyle}>
+          {blog.title}&nbsp;{blog.author}&nbsp;<button onClick={toggleBlogVisible}>hide</button>
+          <br></br><a href={blog.url}>{blog.url}</a>
+          <br></br>likes {blog.likes} <button>like</button>
+          <br></br>{blog.user.name}
+      </div>
+    )
+  } else {
+    return(
+      <div style={blogStyle}>
+        {blog.title} {blog.author} &nbsp;<button onClick={toggleBlogVisible}>view</button>
+      </div>
+    )
+  }
+}
 
 export default Blog
