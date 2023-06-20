cc2a2e67e0addcb0843c58f2e88c71a039408a73
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-06-20 14:54:56 +0300

    finish 5.10 blogilistan frontend, step 10

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -151,7 +151,8 @@ const App = () => {
         />
       </Togglable>
       <div> 
-        {blogs.map(blog =>
+        
+        {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog 
           key={blog.id} 
           blog={blog}
