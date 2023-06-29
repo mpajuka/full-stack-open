273f53b7c9e2246d8d3600756ea53c9eccb5a5ff
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-06-29 22:36:48 +0300

    finish 5.17 blogilistan end to end -testit, step 1

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -27,6 +27,10 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+if (process.env.NODE_ENV === 'test') {
+  const cypressRouter = require('./controllers/cypress')
+  app.use('/api/testing', cypressRouter)
+}
 app.use(middleware.errorHandler)
 
 module.exports = app
--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -41,4 +41,5 @@ blogsRouter.put('/:id', async (request, response) => {
   response.status(200).json(updatedBlog)
 })
 
-module.exports = blogsRouter
+
+module.exports =  blogsRouter
