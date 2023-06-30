866d192f330f7992dda6e7d2a69ad6d3b94874d7
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-06-30 09:39:38 +0300

    finish 5.18 blogilistan end to end -testit, step 2

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +15,21 @@ describe('Blog app', function() {
     cy.contains('username').find('input')
     cy.contains('password').find('input')
     cy.get('form').contains('login')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('username').find('input').type('mluukkai')
+      cy.contains('password').find('input').type('salainen')
+      cy.get('form').contains('login').click()
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('username').find('input').type('mluukkai')
+      cy.contains('password').find('input').type('wrong')
+      cy.get('form').contains('login').click()
+      cy.contains('wrong username or password')
+    })
   })
 })
