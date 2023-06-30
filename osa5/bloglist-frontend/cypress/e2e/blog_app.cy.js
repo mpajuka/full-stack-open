92602dd5d75bd34745604caae118cc5d4594a42a
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-06-30 09:57:46 +0300

    finish 5.19-5.22 blogilistan end to end -testit, steps 3-6

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -32,4 +32,67 @@ describe('Blog app', function() {
       cy.contains('wrong username or password')
     })
   })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.contains('username').find('input').type('mluukkai')
+      cy.contains('password').find('input').type('salainen')
+      cy.get('form').contains('login').click()
+    })
+
+    it('A blog can be created', function() {
+      cy.contains('create a new blog').click()
+      cy.contains('title').find('input').type('test-title')
+      cy.contains('author').find('input').type('test-author')
+      cy.contains('url').find('input').type('test-url')
+      cy.get('form').contains('create').click()
+      cy.contains('a new blog test-title by test-author added')
+    })
+
+    it('A blog can be liked', function() {
+      cy.contains('create a new blog').click()
+      cy.contains('title').find('input').type('test-title')
+      cy.contains('author').find('input').type('test-author')
+      cy.contains('url').find('input').type('test-url')
+      cy.get('form').contains('create').click()
+      cy.get('button').contains('view').click()
+      cy.contains('likes 0')
+      cy.get('button').contains('like').click()
+      cy.contains('likes 1')
+    })
+
+    it('A blog can be removed by the creator', function() {
+      cy.contains('create a new blog').click()
+      cy.contains('title').find('input').type('test-title')
+      cy.contains('author').find('input').type('test-author')
+      cy.contains('url').find('input').type('test-url')
+      cy.get('form').contains('create').click()
+      cy.get('button').contains('view').click()
+      cy.get('button').contains('remove').click()
+      cy.contains('Successfully removed test-title by test-author')
+    })
+
+    it('A blog can only be removed by the creator', function() {
+      cy.contains('create a new blog').click()
+      cy.contains('title').find('input').type('test-title')
+      cy.contains('author').find('input').type('test-author')
+      cy.contains('url').find('input').type('test-url')
+      cy.get('form').contains('create').click()
+      cy.get('button').contains('view').click()
+      cy.get('button').contains('remove')
+      cy.get('button').contains('log out').click()
+      const newUser = {
+        name: 'Arto Hellas',
+        username: 'hellas',
+        password: 'salainen'
+      }
+      cy.request('POST', 'http://localhost:3003/api/users/', newUser)
+      cy.contains('username').find('input').type('hellas')
+      cy.contains('password').find('input').type('salainen')
+      cy.get('form').contains('login').click()
+      cy.contains('Arto Hellas logged in')
+      cy.get('button').contains('view').click()
+      cy.get('button').contains('remove').should('not.exist')
+    })
+  })
 })
