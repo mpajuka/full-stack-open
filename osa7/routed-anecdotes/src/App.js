ab08800f931bb6bdca2d9c456f0951601f702069
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-07-02 23:03:20 +0300

    finish 7.5 anekdoottisovellus ja hookit, step 2

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -78,35 +78,32 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
+
   return (
     <div>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      <form onReset={handleReset} onSubmit={handleSubmit}>
         <div>
           content
-          <input 
-            name='content' 
-            value={content.value} 
-            onChange={content.onChange} 
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input 
-            name='author' 
-            value={author.value} 
-            onChange={author.onChange} 
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input 
-            name='info' 
-            value={info.value} 
-            onChange={info.onChange} 
-          />
+          <input {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='reset'>reset</button>
       </form>
     </div>
   )
