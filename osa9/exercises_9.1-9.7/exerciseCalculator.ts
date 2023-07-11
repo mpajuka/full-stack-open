cbe55d3302fc6e8a18f95c46978607738e206b1e
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-07-11 18:31:16 +0300

    finish 9.2 Exercise calculator

---
+++ osa9/exercises_9.1-9.7/exerciseCalculator.ts
@@ -0,0 +1,37 @@
+interface Result {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number
+}
+
+const calculateExercises = (hours: number[], target: number): Result => {
+  const sum = hours.reduce((a, b) => a + b, 0)
+  const average = sum / hours.length
+  let rating: number
+  let ratingDescription: string
+  if (average >= target) {
+    rating = 3
+    ratingDescription = 'congratulations you met your target!'
+  } else if (average < target && average > target / 2) {
+    rating = 2
+    ratingDescription = 'you did not quite meet the set target, but great work nonetheless!'
+  } else if (average < target / 2) {
+    rating = 1
+    ratingDescription = 'you did not meet the set target, try to do better next time!'
+  }
+  return {
+    periodLength: hours.length,
+    trainingDays: hours.filter(h => h !== 0).length,
+    success: average >= target ? true : false,
+    rating: rating,
+    ratingDescription: ratingDescription,
+    target: target,
+    average: average
+  }
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
