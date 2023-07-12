066c6bb9ad69edc9df26c7c1f09995c59ae39457
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2023-07-12 12:34:34 +0300

    finish 9.5 WebBMI

--- osa9/exercises_9.1-9.7/index.ts
+++ osa9/exercises_9.1-9.7/index.ts
@@ -1,12 +1,31 @@
 import express from 'express';
 
 const app = express();
+import calculateBmi from './calculateBmi';
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+
+  try {
+    const height = Number(req.query.height)
+    const weight = Number(req.query.weight)
+    if (height <= 0 || weight <= 0) {
+      throw new Error
+    }
+    res.status(200).send({
+      weight,
+      height,
+      bmi: calculateBmi(height, weight),
+    })
+  } catch (error: unknown) {
+    res.status(400).send({error: "malformatted parameters"})
+  }
+})
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
